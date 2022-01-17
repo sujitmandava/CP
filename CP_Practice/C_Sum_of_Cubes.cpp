5eaf176f77356e61799ca196a047a07702375a93
Sujit <sujitmandava123@gmail.com>
2022-01-17 18:16:23 +0530

    New problem

--- CP_Practice/C_Sum_of_Cubes.cpp
+++ CP_Practice/C_Sum_of_Cubes.cpp
@@ -8,6 +8,24 @@ using namespace std;
 
 void solve()
 {
+    int_fast64_t n;
+    cin >> n;
+    map<int_fast64_t, int_fast64_t> c;
+    for (int_fast64_t i = 1; i * i * i < n; i++)
+    {
+        c[i*i*i] = i;
+    }
+    for (auto itr = c.begin(); itr != c.end(); itr++)
+    {
+        int_fast64_t k = n - itr->first;
+        if (c.find(k) != c.end())
+        {
+            cout << "YES";
+            return;
+        }
+    }
+    cout << "NO";
+    return;
 }
 
 int main()
@@ -21,7 +39,7 @@ int main()
 
     for (int i = 0; i < test_cases; i++)
     {
-        // cout << "Case #" << i + 1 << ": ";
+        // cout << "Case #" << i + 1 << ": "; // For Google contests
         solve();
         cout << "\n";
     }
